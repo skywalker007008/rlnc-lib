c7caebbc9883c25b257e055471259b69575ce439
skywalker007008 <skywalker007008@163.com>
2019-08-11 20:53:47 +0800

    finish encode and decode

--- codec.cpp
+++ codec.cpp
@@ -12,32 +12,39 @@
 GFType RLNC encode(char* packet, int length, int iter_time, char* buf) {
     // char* buf = (char*)malloc(length * sizeof(char));
     // int windows = ROUND(length, kPacketSize) / kPacketSize;
+    // memcpy(buf, packet, length * sizeof(char));
     memset(buf, 0, length * sizeof(char));
     GFType rand = std::rand() % gFieldSize;
     std::cout << "rand:" << rand << "\n";
-    uint16_t result;
     for (int i = 0; i < iter_time; i++) {
         for (int j = 0; j < length; j++) {
-            result = (rand<<8) + packet[j];
-            printf("%d ", buf[j]);
-            buf[j] ^= gf_mul(HIGH_8(result), LOW_8(result));
+            printf("%x ", (uint8_t)buf[j]);
+            buf[j] = gf_mul(rand, (uint8_t)packet[j]);
             // printf("%d %d\n", HIGH_8(result), LOW_8(result));
             // std::cout << gf_mul(HIGH_8(result), LOW_8(result)) << "\n";
-            printf("%d\n", buf[j]);
+            printf("%x\n", (uint8_t)buf[j]);
         }
     }
     return rand;
 }
 
 void RLNC decode(char* packet, int length, int iter_time, char* buf, GFType rand) {
-
+    // memcpy(buf, packet, length * sizeof (char));
+    memset(buf, 0, length * sizeof(char));
+    for (int i = 0; i < iter_time; i++) {
+        for (int j = 0; j < length; j++) {
+            printf("%x ", (uint8_t)packet[j]);
+            buf[j] = gf_mul(gf_inv(rand), (uint8_t)packet[j]);
+            printf("%x\n", (uint8_t)buf[j]);
+        }
+    }
 }
 
 void RLNC print(char* msg) {
     int len = strlen(msg);
     for (int i = 0; i < len / 8; i++) {
         for (int j = 0; j < 8; j++) {
-            std::cout << msg[i * 8 + j] << "  " ;
+            printf("%x ", (uint8_t)msg[i * 8 + j]);
         }
         std::cout << "\n";
     }
@@ -46,8 +53,19 @@ void RLNC print(char* msg) {
 void RLNC print_mul() {
     for (int i = 0; i < gFieldSize; i++) {
         for (int j = 0; j < gFieldSize; j++) {
-            printf("%d ", table_mul[i][j]);
+            printf("%x ", table_mul[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
+
+    for (int i = 0; i < gFieldSize; i++) {
+        for (int j = 0; j < gFieldSize; j++) {
+            printf("%x ", table_div[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("%x", gf_mul(3,0xE));
+
 }
--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include "codec.h"
 
 int main() {
-    GFType rand;
-    char* encode_buf = (char*)malloc(128 * sizeof(char));
+    GFType rand1, rand2, rand3;
+    char* encode_buf1 = (char*)malloc(128 * sizeof(char));
+    char* encode_buf2 = (char*)malloc(128 * sizeof(char));
+    char* encode_buf3 = (char*)malloc(128 * sizeof(char));
     char* decode_buf = (char*)malloc(128 * sizeof(char));
     std::cout << "Hello, World!" << std::endl;
     gf_init(8, prim_poly[8]);
@@ -18,10 +20,26 @@ int main() {
     char packet[128] = "This is the test code. "
             "And encode and decode this message to test the result right or not";
     RLNC print(packet);
-    rand = RLNC encode(packet, 128, 1, encode_buf);
-    std::cout << "Encode Finished\n";
-    RLNC print(encode_buf);
-    // RLNC decode(encode_buf, 128, 1, decode_buf, rand);
-    // std::cout << "Decode Finished\n";
-    // RLNC print(decode_buf);
+    rand1 = RLNC encode(packet, 128, 1, encode_buf1);
+    std::cout << "Encode1 Finished\n";
+    RLNC print(encode_buf1);
+
+    rand2 = RLNC encode(encode_buf1, 128, 1, encode_buf2);
+    std::cout << "Encode2 Finished\n";
+    RLNC print(encode_buf2);
+
+    rand3 = RLNC encode(encode_buf2, 128, 1, encode_buf3);
+    std::cout << "Encode3 Finished\n";
+    RLNC print(encode_buf3);
+
+    RLNC decode(encode_buf3, 128, 1, decode_buf, gf_mul(gf_mul(rand1, rand2), rand3));
+    std::cout << "Decode Finished\n";
+    RLNC print(decode_buf);
+
+    int a = gf_mul(103, 104);
+    int c = gf_mul(198, a);
+    int b = gf_mul(gf_inv(gf_mul(103, 198)), c);
+    printf("%x %x\n", a, b);
+
+    // RLNC print_mul();
 }
